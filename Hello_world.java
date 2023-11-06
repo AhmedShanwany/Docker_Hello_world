37b59e7de9f6876c0e1e95f5954891711385d1c5
Ahmed El-Shanwany <Ahmed.El-Shanwany@Qcentriseg.com>
2023-11-06 20:52:34 +0200

    modify java file

--- Hello_world.java
+++ Hello_world.java
@@ -1,21 +1,9 @@
-/**
- * 
- */
-package hello_world;
-
-/**
- * @author Ahmed.El-Shanwany
- *
- */
 public class Hello_world {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 	
 		System.out.println("hello world");
-		System.getProperty("java.classpath");
+		//System.getProperty("java.classpath");
 	}
 
 }
